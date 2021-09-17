79d22182faa433da63144bb2c36cfe749b9b74c5
AhmedHassani <ah.jr22@gmail.com>
2021-09-17 18:44:09 +0300

    flask2

--- app.py
+++ app.py
@@ -1,25 +1,65 @@
-from flask import Flask, render_template
+import flask
+from flask import request, jsonify
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
 
-@app.route("/")
-def index():
-    
-    # Load current count
-    f = open("count.txt", "r")
-    count = int(f.read())
-    f.close()
+books = [
+    {
+     'id': 0,
+     'title': 'اشتري الآن وادفع لاحقًا تخفيض',
+     'discount': '20%',
+     'code': "661509a0eca952249cab64dc5770b635",
+     'dec': 'هي خدمة الالكتروني تسهل للعملاء الشراء من موقع نايس عباية بدفع لاحقا بعد 14 يوم من عملية الشراء او بالتقسيط الميسر حسب رغبة العميل بدون اي تكاليف اضافية للعميل عبر منصة شركة تابي'
+    },{
+     'id': 0,
+     'title': 'اشتري الآن وادفع لاحقًا تخفيض',
+     'discount': '30%',
+     'code': "AB4F63F9AC65152575886860DDE480A1",
+     'dec': 'هي خدمة الالكتروني تسهل للعملاء الشراء من موقع نايس عباية بدفع لاحقا بعد 14 يوم من عملية الشراء او بالتقسيط الميسر حسب رغبة العميل بدون اي تكاليف اضافية للعميل عبر منصة شركة تابي'
+    },
+    {
+     'id': 0,
+     'title': 'اشتري الآن وادفع لاحقًا تخفيض',
+     'discount': '15%',
+     'code': "e221d66c9b7d63c038de8332fd9c9aba",
+     'dec': 'هي خدمة الالكتروني تسهل للعملاء الشراء من موقع نايس عباية بدفع لاحقا بعد 14 يوم من عملية الشراء او بالتقسيط الميسر حسب رغبة العميل بدون اي تكاليف اضافية للعميل عبر منصة شركة تابي'
+    },
+    {
+     'id': 0,
+     'title': 'اشتري الآن وادفع لاحقًا تخفيض',
+     'discount': '10%',
+     'code': "998ab57f3c26d6d33e138d84e0300c6a",
+     'dec': 'هي خدمة الالكتروني تسهل للعملاء الشراء من موقع نايس عباية بدفع لاحقا بعد 14 يوم من عملية الشراء او بالتقسيط الميسر حسب رغبة العميل بدون اي تكاليف اضافية للعميل عبر منصة شركة تابي'
+    },
+    {
+     'id': 0,
+     'title': 'اشتري الآن وادفع لاحقًا تخفيض',
+     'discount': '5%',
+     'code': "0be6a9ebe61f379d4fbc9e1479b0757b",
+     'dec': 'هي خدمة الالكتروني تسهل للعملاء الشراء من موقع نايس عباية بدفع لاحقا بعد 14 يوم من عملية الشراء او بالتقسيط الميسر حسب رغبة العميل بدون اي تكاليف اضافية للعميل عبر منصة شركة تابي'
+    },
+    {
+     'id': 0,
+     'title': 'اشتري الآن وادفع لاحقًا تخفيض',
+     'discount': '7%',
+     'code': "1949975a53c477d6590efdf6d2cd2272",
+     'dec': 'هي خدمة الالكتروني تسهل للعملاء الشراء من موقع نايس عباية بدفع لاحقا بعد 14 يوم من عملية الشراء او بالتقسيط الميسر حسب رغبة العميل بدون اي تكاليف اضافية للعميل عبر منصة شركة تابي'
+    },
+]
 
-    # Increment the count
-    count += 1
 
-    # Overwrite the count
-    f = open("count.txt", "w")
-    f.write(str(count))
-    f.close()
 
-    # Render HTML with count variable
-    return render_template("index.html", count=count)
+def get_book(code):
+    for b in books:
+        if b.get("code")==code:
+            return  b
 
-if __name__ == "__main__":
-    app.run()
+@app.route('/', methods=['GET'])
+def api_all():
+    return jsonify(books)
+
+@app.route('/discount/<id>', methods=['GET'])
+def api_find(id):
+    return jsonify(get_book(id))
+
+app.run()
